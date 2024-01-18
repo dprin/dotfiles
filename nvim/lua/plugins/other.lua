cf51894ade3e2469a27e9a7f96bd68f0b972dabb
dprin <thenerfgamer09@gmail.com>
2024-01-18 18:45:03 +0100

    tmp start screen

---
+++ nvim/lua/plugins/other.lua
@@ -0,0 +1,11 @@
+return {
+	"tpope/vim-obsession",
+	{
+		'goolord/alpha-nvim',
+		event = "VimEnter",
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		config = function()
+			require('alpha').setup(require('alpha.themes.dashboard').config)
+		end
+	}
+}
