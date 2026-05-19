9a2a5ac0e0eac5474a80b33d434ea93686c8bc53
dprin <yousefelbakri09@gmail.com>
2026-05-19 18:20:46 +0200

    nixos: lutris

--- nix/modules/gaming.nix
+++ nix/modules/gaming.nix
@@ -39,7 +39,22 @@ in {
       pkgs.wineWow64Packages.wayland
       pkgs.winetricks
 
-      pkgs.lutris
+      (pkgs.lutris.override {
+        # Intercept buildFHSEnv to modify target packages
+        buildFHSEnv = args:
+          pkgs.buildFHSEnv (args
+            // {
+              multiPkgs = envPkgs: let
+                # Fetch original package list
+                originalPkgs = args.multiPkgs envPkgs;
+
+                # Disable tests for openldap
+                customLdap = envPkgs.openldap.overrideAttrs (_: {doCheck = false;});
+              in
+                # Replace broken openldap with the custom one
+                builtins.filter (p: (p.pname or "") != "openldap") originalPkgs ++ [customLdap];
+            });
+      })
 
       pkgs.prismlauncher
 
